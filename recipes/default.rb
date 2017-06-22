61d1eb2e364836afe561b4a08d03b797de4aec9b
Daniel Pardo Rus <danielpardorus@gmail.com>
2017-06-22 13:09:17 +0200

    Refactorizado

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,12 +4,6 @@
 #
 # Copyright (c) 2017 The Authors, All Rights Reserved.
 
-package 'httpd'
-
-file '/var/www/html/index.html' do
-  content '<h1>Welcome Home!</h1>'
-end
-
-service 'httpd' do
-  action [:enable,:start]
-end
+include_recipe "httpd::install"
+include_recipe "httpd::configuration"
+include_recipe "httpd::service"
---
+++ recipes/install.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook Name:: httpd
+# Recipe:: install
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
+
+package 'httpd'
